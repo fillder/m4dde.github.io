2007c63fd4e17429ef1389b03afe310ea3906dd1
Marius S. Riseth <marius.s.riseth@pm.me>
2024-04-24 12:36:12 +0200

    das

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-/*const sections = document.querySelectorAll("#section");
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
 window.addEventListener("scroll", navHighlighter);
+
 function navHighlighter() {
-    let scrollY = window.scrollY
+    let scrollY = window.scrollYOffset;
     sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-
-        const sectionTop = (current.getBoundingClientRect().top + window.scrollY) - 50;
-        sectionId = current.Attribute("section"); 
-
-        if (scrollY > sectionTop && scrollY <= sectionTop) + sectionHeight) {
-        document.querySelector(".navigation a[href*=" + sectionId + "]")
-        .classList.add("active");
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = (current.getBoundingClientRect().top + window.scrollYOffset) - 50;
+    sectionId = current.getAttribute("id");
+    if (
+        scrollY > sectionTop &&
+        scrollY <= sectionTop + sectionHeight
+        ){
+            document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
         } else {
-            document.querySelector(".navigation a[href*=" + sectionId + "]")
-            .classList.remove("active");
-        }
+            document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
     });
-}*/
+}
 
 
 
